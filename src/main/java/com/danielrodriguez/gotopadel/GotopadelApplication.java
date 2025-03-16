388b0d653e13c778211f8e66331998c6e560f760
drbarnk <drbarranko@gmail.com>
2025-03-16 15:00:15 +0000

    Añadido CORS

--- src/main/java/com/danielrodriguez/gotopadel/GotopadelApplication.java
+++ src/main/java/com/danielrodriguez/gotopadel/GotopadelApplication.java
@@ -2,12 +2,31 @@ package com.danielrodriguez.gotopadel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GotopadelApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GotopadelApplication.class, args);
+	}
+
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@SuppressWarnings("null")
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
 	}
 
 }
